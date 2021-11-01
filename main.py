899f1d4f600dc5dc37607ca221e8ef7ffcbe681b
Kamilg-o <74872247+Kamilg-o@users.noreply.github.com>
2021-11-01 11:27:26 +0100

    scrapping offer

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from bs4 import BeautifulSoup
 from requests import get
 
 
-url='https://www.olx.pl/oferty/q-samochody-osobowe/'
+url='https://www.olx.pl/motoryzacja/samochody/bmw/q-samochody-osobowe/'
 
 page= get(url)
 bs = BeautifulSoup(page.content)
+
+for offer in bs.find_all("strong"):
+    print(offer.text)
