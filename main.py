aa72c5198eeec5148ad36f906e21b3f2aba56578
Kamilg-o <74872247+Kamilg-o@users.noreply.github.com>
2021-11-01 23:28:31 +0100

    view :q q

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ url='https://www.olx.pl/motoryzacja/samochody/bmw/q-samochody-osobowe/'
 page= get(url)
 bs = BeautifulSoup(page.content)
 
-for offer in bs.find_all("strong"):
-    print(offer.text)
+for offer in bs.find_all(class_="offer-wrapper"):
+    name = offer.find_all("strong")
+    location = offer.find_all("small",class_="breadcrumb")[1]
+    city = location.find("span").get_text()
+    print(name[0].get_text())
+    print(name[1].get_text(),city)
+    print("")
